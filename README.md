375d097a25b1c509093329e2148adaa2969a081c
joric <iamjoric@gmail.com>
2012-05-31 06:08:20 +0600

    Pushed to version 1.0.0

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ mtgox-ticker
 
 Simple MtGox ticker (Firefox Add-on)
 
+License: MPL2.0 (http://mozilla.org/MPL/2.0/)
+
 Thanks to http://mtgox.com for sponsoring this project!
--- data/widget.js
+++ data/widget.js
@@ -1,5 +1,22 @@
 self.port.on('update', function(param) {
-    var widget = document.getElementById("mtgox-ticker");
-    widget.innerHTML = param.content;
-    self.port.emit('width', {width: widget.offsetWidth, content: param.content});
+    var div = document.getElementById("mtgox-ticker");
+
+    while (div.firstChild)
+        div.removeChild(div.firstChild);
+
+    if (param.icon) {
+        var img = document.createElement('img');
+        img.setAttribute('src', param.icon);
+        div.appendChild(img);
+    }
+
+    if (param.text) {
+        var span = document.createElement('span');
+        span.setAttribute('id', 'last');
+        text = document.createTextNode(param.text);
+        span.appendChild(text);
+        div.appendChild(span);
+    }
+
+    self.port.emit('width', {width: div.offsetWidth, content: div.innerHTML});
 });
